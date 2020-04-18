2497a98445978e3408e65942e5e226fdbe5386c8
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-04-19 05:42:41 +0900

    Fix to unprint caution

--- test/init.sh
+++ test/init.sh
@@ -2,7 +2,7 @@
 
 domain="$1"
 
-defaults delete "$domain"
+defaults delete "$domain" > /dev/null 2>&1
 
 defaults write "$domain" setup-0 -string  helloworld
 defaults write "$domain" setup-1 -integer 123
