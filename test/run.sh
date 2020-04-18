9b54277931a46693b91b707fab6c6073210b2aff
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-04-19 06:11:06 +0900

    Update log string

--- test/run.sh
+++ test/run.sh
@@ -1,46 +1,45 @@
 #!/bin/sh
 
 # Initialize
-
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-cd $SCRIPT_DIR
-cd ..
+cd "$SCRIPT_DIR/.."
+
 domain="net.basd4g.debug"
 
 function dump() {
-  filename=$1
-  echo "info: dump to file '$filename'"
+  filename="$1"
+  echo "Info: Dump to file '$filename'"
   defaults export "$domain" - > "$filename"
 }
 
 dump 1.xml
 
-echo "info: change plist of '$domain'"
 sh test/defaults.sh "$domain"
+echo "Info: Change plist of '$domain'"
 
 dump 2.xml
 
-echo "info: fix changed plist"
+echo "Info: Fix changed plist"
 ./bin/patch-defaults "$domain" 2.xml 1.xml | sh
 
 dump 3.xml
 
 if ! diff 1.xml 3.xml ; then
-  echo  "error: 1.xml != 3.xml"
+  echo  "ERROR: 1.xml != 3.xml"
   exit 1
 fi
 
-echo 'info: change fixed plist'
+echo 'Info: Change fixed plist'
 ./bin/patch-defaults "$domain" 1.xml 2.xml | sh
 
 dump 4.xml
 
 if ! diff 2.xml 4.xml ; then
-  echo  "error: 2.xml != 4.xml"
+  echo  "ERROR: 2.xml != 4.xml"
   exit 1
 fi
 
 rm 1.xml 2.xml 3.xml 4.xml
 
-echo  'info: success!'
+echo  'Info: Success!'
 
